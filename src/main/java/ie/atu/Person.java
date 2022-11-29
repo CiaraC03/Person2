1ebc195ae35138e114cc84da54ce852ef608e6b9
G00402357 <g00402357@atu.ie>
2022-11-29 13:36:41 +0000

    New code

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,44 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Person {
+
+        private String name;
+        private String address;
+        private String number;
+
+        public Person() {
+
+        }
+
+        // we need a constructor to create an instance of the class
+        public Person(String name, String address, String number) {
+            this.name = name;
+            this.address = address;
+            this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
