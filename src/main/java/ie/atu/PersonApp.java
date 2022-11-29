17d07da1e9e69d025aab21a46fe0a986e8d7ef0a
G00402357 <g00402357@atu.ie>
2022-11-29 14:47:22 +0000

    New code added

--- src/main/java/ie/atu/PersonApp.java
+++ src/main/java/ie/atu/PersonApp.java
@@ -6,5 +6,16 @@ public class PersonApp {
         System.out.println(dave.toString());
         Customer peter = new Customer("Dave", "Mervue", "12345", "78999", true);
         System.out.println(peter.toString());
+
+        Customer brian = new Customer();
+        brian.setName("Brian");
+        brian.setAddress("Peru");
+        brian.setNumber("987654");
+
+        System.out.println("Name : "+ brian.getName() + "Address : " + brian.getAddress() + "Number : " + brian.getNumber());
     }
+
+
+
+
 }
